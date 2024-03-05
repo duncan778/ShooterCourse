7e83bc27c0370b567b983b39e6f1e4e72354d114
Pavel Pirogov <maket-city@yandex.ru>
2024-03-05 14:51:09 +0300

    Rigidbody player movement

--- Assets/Scripts/PlayerCharacter.cs
+++ Assets/Scripts/PlayerCharacter.cs
@@ -2,25 +2,30 @@ using UnityEngine;
 
 public class PlayerCharacter : MonoBehaviour
 {
-    [SerializeField] private float speed = 2f;
-    private float inputH;
-    private float inputV;
+
+    [SerializeField] private Rigidbody _rigidbody;
+    [SerializeField] private float _speed = 2f;
+    private float _inputH;
+    private float _inputV;
 
     public void SetInput(float h, float v)
     {
-        inputH = h;
-        inputV = v;
+        _inputH = h;
+        _inputV = v;
     }
 
-    void Update()
+    void FixedUpdate()
     {
         Move();
     }
 
     void Move()
     {
-        Vector3 direction = new Vector3(inputH, 0, inputV).normalized;
-        transform.position += direction * Time.deltaTime * speed;
+        // Vector3 direction = new Vector3(_inputH, 0, _inputV).normalized;
+        // transform.position += direction * Time.deltaTime * _speed;
+
+        Vector3 velocity = (Vector3.forward * _inputV + Vector3.right * _inputH).normalized * _speed;
+        _rigidbody.velocity = velocity;
     }
     
     public void GetMoveInfo(out Vector3 position)
