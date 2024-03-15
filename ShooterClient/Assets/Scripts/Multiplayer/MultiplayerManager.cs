5e4c0d337d5a7e73c08a79c1aecca7a0ae47cb24
Pavel Pirogov <maket-city@yandex.ru>
2024-03-15 18:29:59 +0300

    cleaning

--- ShooterClient/Assets/Scripts/Multiplayer/MultiplayerManager.cs
+++ ShooterClient/Assets/Scripts/Multiplayer/MultiplayerManager.cs
@@ -66,14 +66,10 @@ public class MultiplayerManager : ColyseusManager<MultiplayerManager>
 
     private void CreatePlayer(Player player)
     {
-        // int spawnIndex = Random.Range(0, Level.SpawnPoints.Length);
-        // var position = Level.SpawnPoints[spawnIndex].position;
-
         var playerCharacter = Instantiate(this.player, Vector3.zero, Quaternion.identity);
         player.OnChange += playerCharacter.OnChange;
         room.OnMessage<string>("Restart", playerCharacter.GetComponent<Controller>().Restart);
     }
-
 
     private void CreateEnemy(string key, Player player)
     {
