91b82d356e56f0619d1504a191f0744ad2d3fdad
Pavel Pirogov <maket-city@yandex.ru>
2024-03-21 12:45:11 +0300

    need hands non hiding while headshot

--- ShooterClient/Assets/Scripts/EnemyHead.cs
+++ ShooterClient/Assets/Scripts/EnemyHead.cs
@@ -4,19 +4,18 @@ public class EnemyHead : MonoBehaviour
 {
     [SerializeField] private Transform headshotPoint;
     [SerializeField] private GameObject headShotVFX;
-    [SerializeField] private MeshRenderer headMesh;
+    [SerializeField] private MeshRenderer[] headMeshes;
 
     public void Explosion(Quaternion bulletRotation)
     {
         HideHead(true);
         var vfx = Instantiate(headShotVFX, headshotPoint.position, bulletRotation);
         ParticleSystem.MainModule main = vfx.GetComponent<ParticleSystem>().main;
-        main.startColor = headMesh.material.color;
+        main.startColor = headMeshes[0].material.color;
     }
 
     public void HideHead(bool active)
     {
-        var headMeshes = GetComponentsInChildren<MeshRenderer>();
         foreach (var mesh in headMeshes)
             mesh.enabled = !active;
     }
